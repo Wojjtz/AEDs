f9384d99194a76352d18ec493c64ef2a7b3fee7c
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2023-09-19 12:17:59 -0300

    commit

--- AEDs_ListaDeExercicios/src/aeds_listadeexercicios/n1.java
+++ AEDs_ListaDeExercicios/src/aeds_listadeexercicios/n1.java
@@ -2,30 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ListaExercicios;
+package aeds_listadeexercicios;
 
 /**
  *
- * @author 0057149
+ * @author rodol
  */
-public class n2 {
+public class n1 {
     public static void main(String[] args) {
         int vet[] = {1, 2, 3, 4};
         int start = 0;
-        int maior = 0;
-        System.out.println(imprimirVetor(vet, start, maior));
+        
+        imprimirVetor(vet, start);
     }
     
-    static int imprimirVetor(int v[], int s, int m){
-        if(m == 0){
-            m = v[s];
-        }
+    static void imprimirVetor(int v[], int s){
         if(s < v.length){
-           if(v[s] >= m){
-               m = v[s];
-           }
+           System.out.println(v[s]);
            s++;
+           imprimirVetor(v, s);
         }
-        return imprimirVetor(v, s, m);
     }
 }
