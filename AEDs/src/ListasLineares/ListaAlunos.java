7bd0f0ea55d35e89b1c6bf1fcef9ba1434cc3460
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2023-10-03 10:05:04 -0300

    commit

---
+++ AEDs/src/ListasLineares/ListaAlunos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ListasLineares;
+
+/**
+ *
+ * @author rodol
+ */
+public class ListaAlunos {
+    private String nome;
+    private int ra;
+    
+    public void criarAluno(String nome, int ra){
+        this.nome = nome;
+        this.ra = ra;
+    }
+}
