0b900bedc405205a44e7eb65333112be43fe6250
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2023-10-24 14:56:00 -0300

    commit

---
+++ AEDs/src/Filas/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Filas;
+
+/**
+ *
+ * @author rodol
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> lista = new LinkedList();
+        LinkedList<Integer> lista2 = new LinkedList();
+        
+        lista.addHead(5);
+        lista.addHead(8);
+        lista.addHead(2);
+        lista.addHead(4);
+        
+        lista2.addHead(2);
+        lista2.addHead(6);
+        lista2.addHead(5);
+        lista2.addHead(8);
+        
+        
+       
+        
+    }
+}
